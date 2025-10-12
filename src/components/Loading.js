83d79dbdbd34c48bd38593d24919a4782e8ffa0f
Leo Mancini <mancini@leo.gd>
2025-10-12 00:17:28 -0400

    Updating loading position

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import { InstructionsMessage } from "./Instructions";
 
-const LoadingMessage = styled(InstructionsMessage)``;
+const LoadingMessage = styled(InstructionsMessage)`
+  margin-top: 0;
+`;
 
 export const Loading = ({ message }) => {
   return <LoadingMessage>{message || "Loading..."}</LoadingMessage>;
